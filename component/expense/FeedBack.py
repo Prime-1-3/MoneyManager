2782f902943027ca0a79fb9aef8b0e90892dbae8
Miraz <skmirazrahman21@gmail.com>
2024-03-28 20:48:07 +0600

    Modified expense.py

---
+++ component/expense/FeedBack.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from tkinter import Label
+from DataHandler import DataHandler
+
+
+class Feedback:
+    def __init__(self, window):
+        self.OUTPUT_PATH = Path(__file__).parent.parent.parent
+        self.filename = self.OUTPUT_PATH / "data_store/data.csv"
+        self.expense_source = None
+        self.expense_amount = None
+        self.window = window
+        self.warning_label = Label(window, bg="#28283F", fg="red", font=("Poppins", 12))
+
+    def show_submit_label(self):
+        self.warning_label.place(x=515.0, y=430.0)
+        self.warning_label.config(text="Successfully Submitted!!", fg="white")
+        self.window.after(900, lambda: self.warning_label.place_forget())
+
+    def show_no_source_label(self):
+        self.warning_label.place(x=515.0, y=430.0)
+        self.warning_label.config(text="Please Enter Expense Source", fg="red")
+
+    def show_no_amount_label(self):
+        self.warning_label.place(x=515.0, y=430.0)
+        self.warning_label.config(text="Please Enter Expense Amount", fg="red")
+
+    def show_no_data_label(self):
+        self.warning_label.place(x=515.0, y=430.0)
+        self.warning_label.config(text="Please provide data in both field.",
+                                  fg="red", font=("Poppins", 10))
+
+    def show_input_error_label(self):
+        self.warning_label.place(x=515.0, y=430.0)
+        self.warning_label.config(text="Expense Amount Can Be Only Numbers", fg="red")
+
+    def submit_data(self, e_date, expense_source, expense_amount):
+        if expense_source and expense_amount.isnumeric():
+            data = DataHandler(self.filename)
+            data.data_write([0, e_date, expense_source, expense_amount])
+            self.show_submit_label()
+
+        elif expense_amount and not expense_source:
+            self.show_no_source_label()
+
+        elif not expense_amount and expense_source:
+            self.show_no_amount_label()
+
+        elif not expense_source and not expense_amount:
+            self.show_no_data_label()
+
+        elif not expense_amount.isnumeric():
+            self.show_input_error_label()
---
+++ component/expense/main.py
@@ -0,0 +1,32 @@
+from tkinter import Tk
+from pathlib import Path
+from Navigation import NavigationHandler
+from GUI import ExpenseManagerGUI
+from FeedBack import Feedback
+
+OUTPUT_PATH = Path(__file__).parent.parent.parent
+# ASSETS_PATH = OUTPUT_PATH / Path(r"assets\expense")
+filename = OUTPUT_PATH / Path("data_store/data.csv")
+
+
+class ExpenseManager:
+    def __init__(self):
+        self.window = Tk()
+        self.window.iconbitmap(ExpenseManagerGUI.relative_to_assets("window_logo.ico"))
+        self.window.title("Money Manager")
+        self.window.geometry("1116x582")
+        self.window.geometry("+150+40")
+        self.window.configure(bg="#28283F")
+        self.window.resizable(False, False)
+
+        self.gui = ExpenseManagerGUI(self.window)
+        self.navigation = NavigationHandler(self.window)
+        self.feed = Feedback(self.window)
+
+    def run(self):
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    app = ExpenseManager()
+    app.run()
